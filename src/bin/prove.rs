af8cd1f774c3f7e0f3218ce58ff7baec4aee2550
anylots <842567494@qq.com>
2023-07-10 19:11:22 +0800

    traces save

--- src/bin/prove.rs
+++ src/bin/prove.rs
@@ -1,64 +1,58 @@
 use env_logger::Env;
-use ethers::providers::{Http, Provider};
 use rand::SeedableRng;
 use rand_xorshift::XorShiftRng;
-use std::env::var;
-use std::path::PathBuf;
-use std::str::FromStr;
 use std::fs;
+use std::path::PathBuf;
+use std::time::Duration;
 use zkevm::{
     circuit::{AGG_DEGREE, DEGREE},
-    io,
     prover::Prover,
-    utils::{load_or_create_params, load_or_create_seed},
+    utils,
 };
-use zkevm_prover::utils;
-
 
 #[tokio::main]
 async fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-    //load traces
-    let proof_vec = read_from_file(&path);
-    let proof = serde_json::from_slice::<TargetCircuitProof>(proof_vec.as_slice()).unwrap();
+    //step 1. create prover
+    let mut prover: Prover = create_prover(String::from("prove_params"));
 
-    //step 2. create prover
-    let mut prover = create_prover(params_path);
-
-    //step 3. start prove
-    let block_trace_array = block_traces.as_slice();
-    let result = prover.create_agg_circuit_proof_batch(block_trace_array);
-    match result {
-        Ok(proof) => {
-            log::info!("prove result is: {:#?}", proof);
-            //save proof
-            let mut proof_path = PathBuf::from("./proof").join("agg.proof");
-            fs::create_dir_all(&proof_path).unwrap();
-            proof.write_to_dir(&mut proof_path);
-            //save verify contract
-            let solidity = prover.create_solidity_verifier(&proof);
-            log::info!("verify solidity is: {:#?}", solidity);
-
-            let mut folder = PathBuf::from_str("./verifier").unwrap();
-            io::write_verify_circuit_solidity(&mut folder, &Vec::<u8>::from(solidity.as_bytes()))
-        }
-        Err(e) => {
-            log::info!("prove err: {:#?}", e);
-        }
-    };
+    loop {
+        //step 2. load traces
+        let traces_dir = fs::read_dir("traces/processing").unwrap();
+        let path = traces_dir.last().unwrap().unwrap().path();
+        let traces = utils::get_block_trace_from_file(path.clone());
+        let result = prover.create_agg_circuit_proof_batch(vec![traces].as_slice());
+        //step 3. start prove
+        match result {
+            Ok(proof) => {
+                log::info!("prove result is: {:#?}", proof);
+                //step 4. save proof
+                let mut proof_path = PathBuf::from("proof").join(format!(
+                    "agg.proof#{}",
+                    path.clone().file_name().unwrap().to_str().unwrap()
+                ));
+                proof.write_to_dir(&mut proof_path);
+                fs::rename(path.clone(), PathBuf::from("traces/proved").join(
+                    path.file_name().unwrap().to_str().unwrap())).unwrap();
+            }
+            Err(e) => {
+                log::info!("prove err: {:#?}", e);
+            }
+        };
+        std::thread::sleep(Duration::from_millis(4000))
+    }
 }
 
 /**
  * create prover of zkevm
  */
 fn create_prover(params_path: String) -> Prover {
-
-    let params = load_or_create_params(params_path.as_str(), *DEGREE)
+    let params = utils::load_or_create_params(params_path.as_str(), *DEGREE)
         .expect("failed to load or create kzg params");
-    let agg_params = load_or_create_params(params_path.as_str(), *AGG_DEGREE)
+    let agg_params = utils::load_or_create_params(params_path.as_str(), *AGG_DEGREE)
         .expect("failed to load or create agg-kzg params");
-    let seed = load_or_create_seed("./prove_seed").expect("failed to load or create seed");
+    let seed = utils::load_or_create_seed("./prove_seed").expect("failed to load or create seed");
     let rng = XorShiftRng::from_seed(seed);
 
     Prover::from_params_and_rng(params, agg_params, rng)
--- src/bin/start_server.rs
+++ src/bin/start_server.rs
@@ -12,12 +12,11 @@ use serde::{Deserialize, Serialize};
 use std::fs::{self, File};
 use std::io::{BufReader, Read, Write};
 use std::path::PathBuf;
+use tokio::task;
 use tower_http::add_extension::AddExtensionLayer;
 use tower_http::cors::CorsLayer;
 use tower_http::trace::TraceLayer;
-use zkevm::prover::{AggCircuitProof, Prover};
 use zkevm_prover::utils;
-use tokio::task;
 
 // instead
 #[derive(Serialize, Deserialize, Debug)]
@@ -26,6 +25,8 @@ struct ProveRequest {
     rpc: String,
 }
 
+const PROVER_FOR_TRACES: &'static str = "/target/release/prove";
+
 /**
  * Start Server.
  * this will start an HTTP server, to process zk prove requests.
@@ -33,6 +34,9 @@ struct ProveRequest {
 #[tokio::main]
 async fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    fs::create_dir_all("traces/processing").unwrap();
+    fs::create_dir_all("traces/proved").unwrap();
+    fs::create_dir_all("proof").unwrap();
 
     task::spawn(prove_for_trace());
 
@@ -47,35 +51,47 @@ async fn main() {
         .serve(service.into_make_service())
         .await
         .unwrap();
-
-    // let req_param =String::from("{\"block_num\":4,\"rpc\":\"127.0.0.1:8569\"}");
 }
 
 /**
- * Prove service.
- * Handle Prove Request, Use Layer2's rpc address and block number to fetch trace and generate zk proof.
+ * Download trace.
+ * fetch and save trace from layer2 sequencer.
  */
 async fn download_trace(param: String) -> String {
     //fetch block trace
     let prove_request: ProveRequest = serde_json::from_str(param.as_str()).unwrap();
     let provider = Provider::<Http>::try_from(prove_request.rpc.clone())
         .expect("failed to initialize ethers Provider");
-    let block_traces: Option<Vec<types::eth::BlockTrace>> = utils::get_block_traces_by_number
-    (&provider, prove_request.block_num, prove_request.block_num + 1,).await;
-    if block_traces.is_none(){
-        String::from("fetch block trace fail");
+    let block_traces: Option<Vec<types::eth::BlockTrace>> = utils::get_block_traces_by_number(
+        &provider,
+        prove_request.block_num,
+        prove_request.block_num + 1,
+    )
+    .await;
+    if block_traces.is_none() {
+        return String::from("fetch block trace fail");
     }
+
     //save proof
-    let mut params_file = File::create("trace_path").unwrap();
-    params_file.write_all(serde_json::to_string(&block_traces).unwrap().as_bytes());
-    log::info!("save traces successfully!");
-    String::from("success")
+    let traces_path =
+        PathBuf::from("traces").join(format!("block#{}.json", prove_request.block_num));
+    let mut traces_file = File::create(traces_path).unwrap();
+    let save = traces_file.write_all(serde_json::to_string(&block_traces.unwrap().last()).unwrap().as_bytes());
+    match save {
+        Ok(()) => {
+            log::info!("save traces successfully!");
+            String::from("success")
+        }
+        Err(e) => {
+            log::error!("save trace fail: {e}");
+            String::from("save trace fail")
+        }
+    }
 }
 
 async fn prove_for_trace() {
-    let mut prover_bin = Command::new("./target/release/prove").spawn().unwrap();
+    //start prover process
+    let mut prover_bin = Command::new(PROVER_FOR_TRACES).spawn().unwrap();
     prover_bin.wait().unwrap();
     println!("prover_bin end");
 }
-
-
